e1b20d486eec51cc2eb206b9e2c8fd08d5a92b76
Daniel Trnka <daniel.trnka@gmail.com>
2012-11-18 00:14:21 +0100

    Added spinner for search loading

--- www/assets/javascripts/application.js
+++ www/assets/javascripts/application.js
@@ -7,17 +7,16 @@ $(function () {
 
 $(function() {
 	var timer;
-	$("#video-search").keyup(function(evt) {
+	$("#video-search input").keyup(function(evt) {
 		var self = $(this);
 		clearInterval(timer);
 
 		timer = setTimeout(function() {
-			var query = self.val();
-			if(!query.length < 1) {
-				return;
-			}
+			self.addClass('loading');
 
-			$.nette.ajax({}, self.closest('form'), evt);
+			$.nette.ajax({}, self.closest('form'), evt).done(function() {
+				self.removeClass('loading');
+			});
 			
 		}, 100);
 	});
