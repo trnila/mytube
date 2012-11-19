a053862e9571f03eac385774d94fff4d3a3aaa02
Daniel Trnka <daniel.trnka@gmail.com>
2012-11-19 22:22:57 +0100

    Splitted permissions into methods

--- app/models/Security/Authorizator.php
+++ app/models/Security/Authorizator.php
@@ -6,11 +6,27 @@ class Authorizator extends \Nette\Security\Permission
 {
 	public function __construct(Nette\Security\User $user)
 	{
+		// Add roles
+		$this->addRole('guest');
 		$this->addRole('user');
 		$this->addRole('admin');
 		
+		// Add resources
 		$this->addResource('user');
 
+		// Call all setup methods
+		foreach($this->reflection->methods as $method) {
+			if(Nette\Utils\Strings::startsWith($method->name, 'permit')) {
+				$this->{$method->name}($user);
+			}
+		}
+	}
+
+	/**
+	 * All admins could do everything with user, but they can't deactivate or delete themselves 
+	 */
+	protected function permitUser($user)
+	{
 		$this->allow('admin', 'user', $this::ALL);
 		$this->deny('admin', 'user', 'delete', function($acl) use($user) {
 			return $acl->queriedResource->nickname == $user->id;
