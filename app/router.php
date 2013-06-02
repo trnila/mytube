0f59dd646ea6840f91a4baaf6fa9488b83fbce31
Daniel Trnka <daniel.trnka@gmail.com>
2013-06-02 18:04:02 +0200

    Removed facebook, openid from authentication

--- app/router.php
+++ app/router.php
@@ -1,2 +1,10 @@
 <?php
+use Nette\Application\Routers\Route;
+use Nette\Application\Routers\RouteList;
+
+$container->router[] = $admin = new RouteList('Admin');
+$admin[] = new Route('admin/<presenter>/<action>', 'Dashboard:default');
+
+
+
 $container->router[] = new Nette\Application\Routers\Route('<presenter>/<action>', 'Homepage:default');
