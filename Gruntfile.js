1ee65bd45bddc6bfac173d47d697338cf2b7464b
Daniel Trnka <daniel.trnka@gmail.com>
2013-12-21 14:32:29 +0100

    Grunt: placing bower packages directly into @layout.latte

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,21 @@
 module.exports = function(grunt) {
-	grunt.loadNpmTasks('grunt-contrib-compass');
-	
+	grunt.loadNpmTasks('grunt-bower-install');
+
+	grunt.initConfig({
+		'bower-install': {
+			target: {
+				html: 'app/templates/@layout.latte',
+				ignorePath: 'www',
+				cssPattern: '<link href="{$basePath}{{filePath}}" rel="stylesheet">',
+				jsPattern: '<script src="{$basePath}{{filePath}}"></script>'
+			}
+		}
+
+	});
+
+
+/*	grunt.loadNpmTasks('grunt-contrib-compass');
+
 	grunt.initConfig({
 		compass: {
 			dist: {
@@ -14,5 +29,5 @@ module.exports = function(grunt) {
 		}
 	});
 
-	grunt.registerTask('default', ['compass']);
+	grunt.registerTask('default', ['compass']);*/
 };
